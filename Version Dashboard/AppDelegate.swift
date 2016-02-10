72ddfaa403c38889de157aefcbce718905cdb62d
Christian Schneider <christian@christians-macbook-pro.fritz.box>
2016-02-10 11:43:40 +0100

    Initial Commit

---
+++ Version Dashboard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Version Dashboard
+//
+//  Created by Christian Schneider on 10.02.16.
+//  Copyright © 2016 NonameCompany. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Version Dashboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Version Dashboard
+//
+//  Created by Christian Schneider on 10.02.16.
+//  Copyright © 2016 NonameCompany. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
